33d5315ddd13df8a1939a6e12159544f1afe5837
rhazari <rhazari@g.clemson.edu>
2017-11-05 19:55:35 -0800

    removing outdated files

--- lib/DateTime.cpp
+++
@@ -1,8 +0,0 @@
-#include "DateTime.h"
-
-std::string Time::toString()
-{
-	std::string str;
-	str.append(std::to_string(m_hrs)).append(":").append(std::to_string(m_mins)).append(":").append(std::to_string(m_secs));
-	return str;	
-}
