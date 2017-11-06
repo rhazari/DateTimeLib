8fbaae349df358d53dcec5fbea048fdf7557461a
rhazari <rhazari@g.clemson.edu>
2017-11-05 19:53:30 -0800

    updated project with conan

---
+++ gtest/DateTimeTest.cpp
@@ -0,0 +1,311 @@
+#include "gtest/gtest.h"
+#include "DateTime.h"
+
+using ::testing::Test;
+
+//// return type - vector<KString> {Day, Month, Date, Hour, Minute, Seconds, Year}
+//std::vector<KString> tokenizeDateTimeString(std::string _dateTime)
+//{
+//    auto _vec = tokenize(_dateTime, " ");
+//    if (_vec.size() == 6)
+//        _vec.erase(_vec.begin() + 2);
+//    auto _time = tokenize(_vec[4], ":");
+//    _vec.erase(_vec.begin() + 4);
+//    _vec.insert(_vec.begin() + 4, _time.begin(), _time.end());
+//    return _vec;
+//}
+
+class DateTimeProtectedHelperClass : public DateTime
+{
+public:
+    DateTimeProtectedHelperClass(const DateTime& obj) : DateTime(obj)
+    {
+    }
+
+    time_t getEpochTime()
+    {
+        return m_sinceEpoch;
+    }
+
+    std::tm gettm()
+    {
+        return m_tm;
+    }
+
+    std::string tz()
+    {
+        std::stringstream ss;
+        ss << std::put_time(&m_tm, "%z");
+        return ss.str();
+    }
+};
+
+TEST(DateTime, DateTimeObject)
+{
+    // Date Object
+    {
+        DateTime dt({9, 7, 2017});
+        auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+        EXPECT_EQ(dt.toString(), "2017-09-07T00:00:00" + _timeZone);
+    }
+    // Date Time Object
+    {
+        DateTime dt({9, 7, 2017}, {15, 11, 10});
+        auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+        EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+    }
+}
+
+TEST(DateTime, DateTimeObjectConstructorwithInvalidValues)
+{
+    // Invalid Month
+    {
+        EXPECT_ANY_THROW(DateTime dt({13, 7, 2017}));
+        EXPECT_ANY_THROW(DateTime dt({-3, 7, 2017}));
+    }
+    // Invalid Day
+    {
+        EXPECT_ANY_THROW(DateTime dt({7, 32, 2017}));
+        EXPECT_ANY_THROW(DateTime dt({7, -2, 2017}));
+    }
+    // Invalid Year
+    {
+        EXPECT_ANY_THROW(DateTime dt({7, 17, 1899}));
+        EXPECT_ANY_THROW(DateTime dt({7, 17, -2017}));
+    }
+    // Invalid Hours
+    {
+        EXPECT_ANY_THROW(DateTime dt({9, 7, 2017}, {24, 11, 10}));
+        EXPECT_ANY_THROW(DateTime dt({9, 7, 2017}, {-1, 11, 10}));
+    }
+    // Invalid Minutes
+    {
+        EXPECT_ANY_THROW(DateTime dt({9, 7, 2017}, {15, 60, 10}));
+        EXPECT_ANY_THROW(DateTime dt({9, 7, 2017}, {15, -1, 10}));
+    }
+    // Invalid Seconds
+    {
+        EXPECT_ANY_THROW(DateTime dt({9, 7, 2017}, {15, 11, 61}));
+        EXPECT_ANY_THROW(DateTime dt({9, 7, 2017}, {-5, 11, -1}));
+    }
+}
+
+TEST(DateTime, DateTimeObjectCopyConstructor)
+{
+    // Date Object
+    {
+        DateTime dt({9, 7, 2017});
+        DateTime dt1(dt);
+        EXPECT_EQ(dt1, dt);
+    }
+    // Date Time Object
+    {
+        DateTime dt({9, 7, 2017}, {15, 11, 10});
+        DateTime dt1(dt);
+        EXPECT_EQ(dt1, dt);
+    }
+}
+
+TEST(DateTime, DateTimeObjectCopyAssignment)
+{
+    // Date Object
+    {
+        DateTime dt({9, 7, 2017});
+        DateTime dt1 = dt;
+        EXPECT_EQ(dt1, dt);
+    }
+    // Date Time Object
+    {
+        DateTime dt({9, 7, 2017}, {15, 11, 10});
+        DateTime dt1 = dt;
+        EXPECT_EQ(dt1, dt);
+    }
+}
+
+TEST(DateTime, operators)
+{
+    // Operator ==
+    {
+        DateTime dt1({9, 7, 2017}, {15, 11, 10});
+        DateTime dt2({9, 7, 2017}, {15, 11, 10});
+        EXPECT_EQ(dt1, dt2);
+    }
+    // Operator !=
+    {
+        DateTime dt1({9, 7, 2017}, {15, 11, 10});
+        DateTime dt2 = DateTime::currentLocalTime();
+        EXPECT_NE(dt1, dt2);
+    }
+    // Operator < & >
+    {
+        DateTime dt1({9, 7, 2017}, {15, 11, 10});
+        DateTime dt2({9, 7, 2017}, {15, 11, 11});
+        EXPECT_TRUE(dt1 < dt2);
+        EXPECT_FALSE(dt1 > dt2);
+    }
+    // Operator -
+    {
+        DateTime dt1({9, 7, 2017}, {15, 11, 15});
+        DateTime dt2({9, 7, 2017}, {15, 11, 10});
+        EXPECT_EQ(dt1 - dt2, 5);
+
+        DateTime dt3({9, 9, 2017}, {15, 11, 10});
+        DateTime dt4({9, 7, 2017}, {15, 11, 10});
+        EXPECT_EQ(dt3 - dt4, 2 * 24 * 60 * 60);
+    }
+}
+
+TEST(DateTime, DateObject)
+{
+    DateTime dt({9, 7, 2017}, {15, 11, 10});
+    EXPECT_EQ(dt.getDate().toString(), "9-7-2017");
+}
+
+TEST(DateTime, TimeObject)
+{
+    DateTime dt({9, 7, 2017}, {15, 11, 10});
+    EXPECT_EQ(dt.getTime().toString(), "15:11:10");
+}
+
+//TEST(DateTime, currentLocalTime)
+//{
+//    auto dt = DateTime::currentLocalTime();
+//    time_t t = time(nullptr);
+//    std::tm tm = *std::localtime(&t);
+//    std::stringstream ss;
+//    ss << std::put_time(&tm, "%a %b %d %Y %T");
+//    auto v1 = tokenizeDateTimeString(ss.str());
+//    auto v2 = tokenizeDateTimeString(dt.toString("%a %b %d %Y %T"));
+//    EXPECT_EQ(v1[0], v2[0]);
+//    EXPECT_EQ(v1[1], v2[1]);
+//    EXPECT_EQ(v1[2], v2[2]);
+//    EXPECT_EQ(v1[3], v2[3]);
+//    EXPECT_EQ(v1[4], v2[4]);
+//    EXPECT_EQ(v1[5], v2[5]);
+//}
+
+//TEST(DateTime, currentUTCTime)
+//{
+//    auto dt = DateTime::currentUTCTime();
+//    time_t t = time(nullptr);
+//    std::tm tm = *std::gmtime(&t);
+//    std::stringstream ss;
+//    ss << std::put_time(&tm, "%a %b %d %Y %T");
+//    auto v1 = tokenizeDateTimeString(ss.str());
+//    auto v2 = tokenizeDateTimeString(dt.toString("%a %b %d %Y %T"));
+//    EXPECT_EQ(v1[0], v2[0]);
+//    EXPECT_EQ(v1[1], v2[1]);
+//    EXPECT_EQ(v1[2], v2[2]);
+//    EXPECT_EQ(v1[3], v2[3]);
+//    EXPECT_EQ(v1[4], v2[4]);
+//    EXPECT_EQ(v1[5], v2[5]);
+//}
+
+TEST(DateTime, toString)
+{
+    DateTime dt({9, 7, 2017}, {15, 11, 10});
+    auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+
+    EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+
+    EXPECT_EQ(dt.toString("%Y-%m-%d %T"), "2017-09-07 15:11:10");
+
+    EXPECT_EQ(dt.toString("%Y-%m-%d %I:%M:%S %p"), "2017-09-07 03:11:10 PM");
+
+    EXPECT_EQ(dt.toString("%F %T"), "2017-09-07 15:11:10");
+}
+
+TEST(DateTime, fromString)
+{
+    auto dt = DateTime::fromString("2017-09-07T15:11:10");
+    auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+
+    EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+
+    dt = DateTime::fromString("Sep 07 2017 15:11:10", "%b %d %Y %T");
+    EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+}
+
+TEST(DateTime, addSeconds)
+{
+    DateTime dt({9, 7, 2017}, {15, 11, 10});
+    auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+
+    EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+
+    auto dt1 = dt.addSeconds(2000);
+    EXPECT_EQ(dt1.toString(), "2017-09-07T15:44:30" + _timeZone);
+}
+
+TEST(DateTime, addDays)
+{
+    DateTime dt({9, 7, 2017}, {15, 11, 10});
+    auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+
+    EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+
+    auto dt1 = dt.addDays(10);
+    EXPECT_EQ(dt1.toString(), "2017-09-17T15:11:10" + _timeZone);
+}
+
+TEST(DateTime, addMonths)
+{
+    DateTime dt({9, 7, 2017}, {15, 11, 10});
+    auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+
+    EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+
+    auto dt1 = dt.addMonths(10);
+    EXPECT_EQ(dt1.toString(), "2018-07-07T15:11:10" + _timeZone);
+}
+
+TEST(DateTime, addYears)
+{
+    DateTime dt({9, 7, 2017}, {15, 11, 10});
+    auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+
+    EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+
+    auto dt1 = dt.addYears(3);
+    EXPECT_EQ(dt1.toString(), "2020-09-07T15:11:10" + _timeZone);
+}
+
+TEST(DateTime, toUTCTime)
+{
+    DateTime dt({9, 7, 2017}, {15, 11, 10});
+    auto _timeZone = DateTimeProtectedHelperClass(dt).tz();
+
+    EXPECT_EQ(dt.toString(), "2017-09-07T15:11:10" + _timeZone);
+    auto dt1 = dt.toUTC();
+
+    auto _time = DateTimeProtectedHelperClass(dt).getEpochTime();
+    // Converts time since epoch as std::time_t value into UTC time
+    auto _tm = *gmtime(&_time);
+    std::stringstream ss;
+    ss << std::put_time(&_tm, "%a %b %d %Y %T");
+
+    EXPECT_EQ(dt1.toString("%a %b %d %Y %T"), ss.str());
+}
+
+TEST(DateTime, timeZone)
+{
+    auto dt = DateTime::currentLocalTime();
+
+    std::stringstream ss;
+    auto _tm = DateTimeProtectedHelperClass(dt).gettm();
+    ss << std::put_time(&_tm, "%z");
+
+    EXPECT_EQ(dt.timeZone(), "GMT" + ss.str());
+}
+
+TEST(DateTime, offsetFromUtc)
+{
+    DateTime dt = DateTime::currentLocalTime();
+
+    auto rawtime = DateTimeProtectedHelperClass(dt).getEpochTime();
+    std::tm ptm = *gmtime(&rawtime);
+    // Request that mktime() looksup dst in timezone database
+    ptm.tm_isdst = -1;
+    time_t gmt = mktime(&ptm);
+    EXPECT_EQ(dt.offsetFromUtc(), (int)difftime(rawtime, gmt));
+}
---
+++ src/DateTime.h
@@ -0,0 +1,203 @@
+#pragma once
+#include <assert.h>
+#include <ctime>
+#include <iomanip>
+#include <string>
+#include <stdexcept>
+#include <sstream>
+
+class Time
+{
+    friend class DateTime;
+    int m_hrs;
+    int m_mins;
+    int m_secs;
+
+public:
+    Time() : m_hrs(0), m_mins(0), m_secs(0)
+    {
+    }
+
+    Time(int hrs, int mins, int secs) : m_hrs(hrs), m_mins(mins), m_secs(secs)
+    {
+    }
+
+    std::string toString()
+    {
+        std::string str;
+        str.append(std::to_string(m_hrs)).append(":").append(std::to_string(m_mins)).append(":").append(std::to_string(m_secs));
+        return str; 
+    }
+};
+
+class Date
+{
+    friend class DateTime;
+    int m_day;
+    int m_month;
+    int m_year;
+
+public:
+    Date(int _month, int _day, int _year) : m_month(_month), m_day(_day), m_year(_year)
+    {
+    }
+
+    std::string toString()
+    {
+        std::string str;
+        str.append(std::to_string(m_month)).append("-").append(std::to_string(m_day)).append("-").append(std::to_string(m_year));
+        return str;
+    }
+};
+
+/**
+* @class DateTime DateTime.h "Keysight/Cdm/Experimental/DateTime.h"
+* @brief Class for creating a DateTime object
+* @parameters Month, Day, Year, Hours, Minutes, Seconds
+*/
+class DateTime
+{
+protected:
+    Date m_date;
+    Time m_time;
+    time_t m_sinceEpoch;
+    std::tm m_tm;
+
+public:
+    DateTime(Date _date, Time _time = Time()) : m_date(_date), m_time(_time)
+    {
+        // Date
+        m_tm.tm_mon = m_date.m_month - 1;
+        m_tm.tm_mday = m_date.m_day;
+        m_tm.tm_year = m_date.m_year - 1900;
+        // Time
+        m_tm.tm_hour = m_time.m_hrs;
+        m_tm.tm_min = m_time.m_mins;
+        m_tm.tm_sec = m_time.m_secs;
+        m_sinceEpoch = std::mktime(&m_tm);
+        if (m_tm.tm_mon != m_date.m_month - 1 || m_tm.tm_mday != m_date.m_day || m_tm.tm_year != m_date.m_year - 1900 ||
+            m_tm.tm_hour != m_time.m_hrs || m_tm.tm_min != m_time.m_mins || m_tm.tm_sec != m_time.m_secs || m_sinceEpoch == -1)
+            throw std::runtime_error("Invalid Date Time value for the constructor");
+    }
+
+    DateTime(const DateTime&) = default;
+    DateTime& operator=(const DateTime&) = default;
+
+    std::string toString(std::string format = "%FT%T%z")
+    {
+        std::stringstream ss;
+        ss << std::put_time(&m_tm, format.data());
+        return ss.str();
+    }
+
+    Time getTime()
+    {
+        return m_time;
+    }
+
+    Date getDate()
+    {
+        return m_date;
+    }
+
+    DateTime addSeconds(int seconds)
+    {
+        std::tm tm = this->m_tm;
+        tm.tm_sec += seconds;
+        std::mktime(&tm);
+        return DateTime({tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900}, {tm.tm_hour, tm.tm_min, tm.tm_sec});
+    }
+
+    DateTime addDays(int days)
+    {
+        return addSeconds(days * 24 * 60 * 60);
+    }
+
+    DateTime addMonths(int months)
+    {
+        std::tm tm = this->m_tm;
+        tm.tm_mon += months;
+        std::mktime(&tm);
+        return DateTime({tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900}, {tm.tm_hour, tm.tm_min, tm.tm_sec});
+    }
+
+    DateTime addYears(int years)
+    {
+        std::tm tm = this->m_tm;
+        tm.tm_year += years;
+        std::mktime(&tm);
+        return DateTime({tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900}, {tm.tm_hour, tm.tm_min, tm.tm_sec});
+    }
+
+    DateTime toUTC()
+    {
+        time_t temp_time_t = this->m_sinceEpoch;
+        std::tm tm = *gmtime(&temp_time_t);
+        return DateTime({tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900}, {tm.tm_hour, tm.tm_min, tm.tm_sec});
+    }
+
+    int offsetFromUtc()
+    {
+        time_t rawtime = this->m_sinceEpoch;
+        struct tm ptm = *gmtime(&rawtime);
+        // Request that mktime() looksup dst in timezone database
+        ptm.tm_isdst = -1;
+        time_t gmt = mktime(&ptm);
+        return (int)difftime(rawtime, gmt);
+    }
+
+    std::string timeZone()
+    {
+        std::stringstream ss;
+        ss << std::put_time(&m_tm, "%z");
+        return "GMT" + ss.str();
+    }
+
+    static DateTime currentLocalTime()
+    {
+        std::time_t t = std::time(nullptr);
+        std::tm tm = *localtime(&t);
+        return DateTime({tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900}, {tm.tm_hour, tm.tm_min, tm.tm_sec});
+    }
+
+    static DateTime currentUTCTime()
+    {
+        std::time_t t = std::time(nullptr);
+        std::tm tm = *gmtime(&t);
+        return DateTime({tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900}, {tm.tm_hour, tm.tm_min, tm.tm_sec});
+    }
+
+    static DateTime fromString(std::string dateTime, std::string format = "%Y-%m-%dT%H:%M:%S%z")
+    {
+        std::tm tm;
+        std::istringstream ss(dateTime);
+        ss >> std::get_time(&tm, format.data());
+        return DateTime({tm.tm_mon + 1, tm.tm_mday, tm.tm_year + 1900}, {tm.tm_hour, tm.tm_min, tm.tm_sec});
+    }
+
+    // Operators
+    bool operator==(DateTime rhs) const
+    {
+        return this->m_sinceEpoch == rhs.m_sinceEpoch;
+    }
+
+    bool operator!=(DateTime rhs) const
+    {
+        return this->m_sinceEpoch != rhs.m_sinceEpoch;
+    }
+
+    bool operator<(DateTime rhs) const
+    {
+        return this->m_sinceEpoch < rhs.m_sinceEpoch;
+    }
+
+    bool operator>(DateTime rhs) const
+    {
+        return this->m_sinceEpoch > rhs.m_sinceEpoch;
+    }
+
+    int operator-(DateTime rhs) const
+    {
+        return (int)(this->m_sinceEpoch - rhs.m_sinceEpoch);
+    }
+};
